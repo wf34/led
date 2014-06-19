622ee24505b4455f97f3b085d4869db494d8e078
wf34 <wf34@users.noreply.github.com>
2014-06-20 01:26:03 +0400

    getter part of the client

--- client/cli.sh
+++ client/cli.sh
@@ -1,5 +1,52 @@
 #!/bin/bash
 set -x
+
+cmdGetState="get-led-state"
+cmdGetColor="get-led-color"
+cmdGetFreq="get-led-rate"
+
+function promptErr() {
+    echo "Wrong input"
+    continue
+}
+
+function switchVars() {
+    stateHandler=$1
+    freqHandler=$2
+    colorHandler=$3
+    echo "What are you interested at? State, Freq, Color? [S] [F] [C]"
+    read key
+    if [ "$key" == "S" ]; then
+        $stateHandler 
+    elif [ "$key" == "F" ]; then
+        $freqHandler
+    elif [ "$key" == "C" ]; then
+        $colorHandler
+    else
+        promptErr
+    fi
+}
+
+function sendAndRecv() {
+    request=$1
+    echo "$request" > "${fifoReq}"
+    response=$(timeout 1s cat "${fifoResp}")
+    echo $response
+}
+
+function getState() {
+    sendAndRecv "$cmdGetState"
+}
+
+function getFreq() {
+    sendAndRecv "$cmdFreqState"
+}
+
+function getColor() {
+    sendAndRecv "$cmdColorState"
+}
+
+
 fifoDir="/tmp/led"
 id=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 fifoReq="${fifoDir}/led_pipe_${id}_req"
@@ -12,18 +59,20 @@ mkfifo "${fifoResp}"
 echo "${fifoReq}"
 echo "${fifoResp}"
 
-echo 'kuku' > "${fifoReq}"
-response=`timeout 1s cat "${fifoResp}"
-`
 
-#read "${fifoResp}"
+while true
+do
+    echo "Get or Set? [G] [S]"
+    read keyGS
+    if [ "$keyGS" == "G" ]; then
+        switchVars getState getFreq getColor
+    elif [ "$keyGS" == "S" ]; then
+        switchVars setState setFreq setColor
+    else
+        promptErr
+    fi
+done
 
-#read -t 1 line < "$fifoResp"
-#read -t 2 <>"$fifoResp"
 
-echo $response
-
-#rm("${fifoReq}")
-#rm("${fifoResp}")
 
 
